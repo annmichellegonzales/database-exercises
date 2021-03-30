e07553b8c5736b67e578dbc6af7683a0d8aea50b
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-03-30 14:40:42 -0500

    update exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,7 +1,7 @@
 USE codeup_test_db;
 -- SELECT * FROM albums WHERE artist = 'Pink Floyd';
--- SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+-- SELECT release_date FROM albums WHERE name LIKE 'Sgt. Pepper%';
 -- SELECT genre FROM albums WHERE name = 'Nevermind';
 -- SELECT name FROM albums WHERE release_date >= 1990 AND release_date < 2000;
 -- SELECT name FROM albums WHERE sales < 20.0;
-SELECT name FROM albums WHERE genre = 'Rock';
+-- SELECT name FROM albums WHERE genre LIKE '%Rock%';
