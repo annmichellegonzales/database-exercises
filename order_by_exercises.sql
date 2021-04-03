82b4bb68ce4ea14f61b510549912e38912d2eebc
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-04-02 22:32:05 -0500

    organizing exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,9 +1,22 @@
 USE employess;
 
+-- SELECT CONCAT(first_name, ' ', last_name) AS 'Full Name' FROM employees LIMIT 100;
+--
+-- SELECT DAY(birth_date), birth_date
+-- FROM employees
+-- LIMIT 100;
+
+-- SELECT NOW;
+
+-- INSERT INTO myTable (name, created_date)
+-- VALUES ('Casey', NOW())
+
 -- SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
 
--- SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY last_name DESC;
---
+-- SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%' ORDER BY last_name DESC;
+
+SELECT * FROM emloyees WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25;
+
 -- SELECT * FROM employees WHERE last_name LIKE '%q%';
 --
 -- SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
