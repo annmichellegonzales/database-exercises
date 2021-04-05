136ff0cf7dc69a8200536d33cb0aa465230b2dad
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-04-05 13:34:25 -0500

    created join_test_db

---
+++ warmup.sql
@@ -0,0 +1,25 @@
+-- What is the proper order of the following components of a DQL query?
+--
+-- SELECT * FROM adsf WHERE fdsa group by jkl; order by ;lkj limit blAH offset halb;
+--
+--     What is a Key/Index in MySQL?
+--
+--     primary keys are typically id's that are set to increment. primary keys are not null. there are also foreign keys.
+--
+--     What is the purpose of a Primary Key? A Foreign Key? A Unique Key?
+
+
+--     Consider the following columns for a pizza-ordering app table. How might this information be broken up over multiple tables?
+--     pizza_orders table...
+--
+--     CUSTOMER PROFILE                ORDER INFO
+-- -----------------------            ------------------
+--
+--     id                             id
+--     name_of_user                   datetime_of_order
+--     email_of_user                  datetime_of_delivery
+--     address_of_user                 order_description
+
+
+
+
